5b57d2c8e259b4b9b20ce5e3405a0606fd040a28
Connor Campbell <connor.campbell@accenture.com>
2022-10-07 12:02:09 +0100

    player class made

---
+++ src/main/java/com/example/whosplaying/WhosplayingApplication.java
@@ -0,0 +1,13 @@
+package com.example.whosplaying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhosplayingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhosplayingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whosplaying/WhosplayingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whosplaying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhosplayingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
