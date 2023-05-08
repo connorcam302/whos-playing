447b32f1d1ba97870133bc1c16286d6f2d551696
Connor Campbell <connorcam123@gmail.com>
2023-05-08 17:33:33 +0100

    Mobile overhaul

--- src/components/MatchCard.js
+++ src/components/MatchCard.js
@@ -15,94 +15,100 @@ import {
   Box,
   Center,
   Tooltip,
+  Wrap,
 } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function MatchCard(props) {
-    return (
-      <Card width="15em" bgColor={props.match.winner == true? "#0b3014" : "#471210"}>
-        <Image
-          objectFit="cover"
-          width="70em"
-          src={props.match.hero.img}
-          alt={props.match.hero.name}
-        />
-        <CardBody paddingTop="0.5em">
-          <Stack spacing="3">
-            <Flex>
-              <Center>
-                <Heading size="md">{props.match.username}</Heading>
-              </Center>
-              <Spacer />
-              <Box>
-                <Heading size="md" textAlign="right">
-                  {makeWinnerText(props.match.winner)}
-                  {props.match.kills}/{props.match.deaths}/
-                  {props.match.assists}
-                </Heading>
-              </Box>
-            </Flex>
-            <Flex>
-              {makeItemBox(props.match.items[0])}
-              {makeItemBox(props.match.items[1])}
-              {makeItemBox(props.match.items[2])}
-            </Flex>
-            <Flex style={{ marginTop: 0 }}>
-              {makeItemBox(props.match.items[3])}
-              {makeItemBox(props.match.items[4])}
-              {makeItemBox(props.match.items[5])}
-            </Flex>
-            <Text textAlign="right">
-              {calculateTime(
-                props.match.start_time,
-                props.match.duration
-              )}
-            </Text>
-          </Stack>
-        </CardBody>
-        <Divider />
-        <Box padding="20px" bg="#1e1f2e" borderBottomRadius="6px">
-          <Flex>
-            <Text color="#ef3a1b" fontWeight="bold">
-              <Link
-                href={
-                  "https://www.dotabuff.com/matches/" +
-                  props.match.match_id
-                }
-                passHref
-                legacyBehavior
-              >
-                <a target="_blank" rel="noopener noreferrer">
-                  Dotabuff
-                </a>
-              </Link>
-            </Text>
-            <Spacer />
-            <Text color="#00c59e" fontWeight="bold">
-              <Link
-                href={
-                  "https://www.opendota.com/matches/" +
-                  props.match.match_id
-                }
-                passHref
-                legacyBehavior
-              >
-                <a target="_blank" rel="noopener noreferrer">
-                  OpenDota
-                </a>
-              </Link>
-            </Text>
-          </Flex>
+  return (
+    <Box width='24em' bgColor={props.match.winner == true ? "#0b3014" : "#471210"} borderRadius='10px 10px 0 10px'>
+      <Wrap spacing={5} margin={2}>
+        <Center>
+          <Link href={"/player/" + props.match.player_id} passHref legacyBehavior>
+            <a rel='noopener noreferrer'>
+              <Flex w="3em">
+                <Center>
+                  <Heading size='md'>{props.match.username}</Heading>
+                </Center>
+                <Spacer />
+              </Flex>
+            </a>
+          </Link>
+        </Center>
+        <Spacer />
+        <Center>
+          <Text textAlign='right'>{makeWinnerText(props.match.winner)}</Text>
+        </Center>
+        <Spacer />
+        <Center>
+          <Text textAlign='right'>
+            {props.match.kills}/{props.match.deaths}/{props.match.assists}
+          </Text>
+        </Center>
+        <Spacer />
+        <Center>
+          <Text textAlign='center'>
+            {Math.floor(props.match.duration / 60)}:
+            {Math.round(props.match.duration % 60) > 10 ? Math.round(props.match.duration % 60) : "0" + Math.round(props.match.duration % 60)}
+          </Text>
+        </Center>
+      </Wrap>
+      <Divider marginBottom={2} />
+      <Wrap spacing={0}>
+        <Box>
+          <Wrap spacing={0}>
+            <Image height='3.65em' src={props.match.hero.img} alt={props.match.hero.name} sx={{ margin: 0 }} />
+            <Box w={5} />
+            <Box>
+              <Wrap>
+                <Box>
+                  <Flex>
+                    {makeItemBox(props.match.items[0])}
+                    {makeItemBox(props.match.items[1])}
+                    {makeItemBox(props.match.items[2])}
+                  </Flex>
+                  <Flex style={{ marginTop: 0 }}>
+                    {makeItemBox(props.match.items[3])}
+                    {makeItemBox(props.match.items[4])}
+                    {makeItemBox(props.match.items[5])}
+                  </Flex>
+                </Box>
+                <Center>
+                  <Box w='2em'>{makeNeutralBox(props.match.items[6])}</Box>
+                </Center>
+                <Center>
+                  <Box w='2.3em'>{makeAghBox(props.match.aghanims_scepter, props.match.aghanims_shard)}</Box>
+                </Center>
+                <Box paddingRight='10px' paddingLeft='10px'>
+                  <Text color='#ef3a1b' fontWeight='bold' textAlign='right'>
+                    <Link href={"https://www.dotabuff.com/matches/" + props.match.match_id} passHref legacyBehavior>
+                      <a target='_blank' rel='noopener noreferrer'>
+                        <Image src='/dotabuff-logo.png' alt='Dotabuff' boxSize='1.5em' display='inline-block' verticalAlign='middle' />
+                      </a>
+                    </Link>
+                  </Text>
+                  <Text color='#00c59e' fontWeight='bold' textAlign='right'>
+                    <Link href={"https://www.opendota.com/matches/" + props.match.match_id} passHref legacyBehavior>
+                      <a target='_blank' rel='noopener noreferrer'>
+                        <Image src='/opendota-logo.png' alt='OpenDota' boxSize='1.5em' display='inline-block' verticalAlign='middle' />
+                      </a>
+                    </Link>
+                  </Text>
+                </Box>
+              </Wrap>
+            </Box>
+          </Wrap>
         </Box>
-      </Card>
-    );
+      </Wrap>
+    </Box>
+  );
 }
 
 function makeWinnerText(bool) {
   if (bool) {
-    return <Text color="green">WIN</Text>;
+    return <Text color='green'>WIN</Text>;
   } else {
-    return <Text color="red">LOSS</Text>;
+    return <Text color='red'>LOSS</Text>;
   }
 }
 
@@ -127,12 +133,58 @@ function calculateTime(epoch, duration) {
 
 function makeItemBox(item) {
   if (item.img === null) {
-    return <Box bg="radial-gradient(#8D99AE, #212121);" w="4em" />;
+    return <Box bg='radial-gradient(#8D99AE, #212121);' w='2em' />;
   } else {
     return (
       <Tooltip label={item.name}>
-        <Box bg="#212121" w="4em">
-          <Image src={item.img} alt={item.name}/>
+        <Box bg='#212121' w='2.5em'>
+          <Image src={item.img} alt={item.name} />
+        </Box>
+      </Tooltip>
+    );
+  }
+}
+
+function makeAghBox(scepter, shard) {
+  if (scepter == 0 && shard == 0) {
+    return (
+      <Box>
+        <Image src='/scepter_0.png' />
+        <Image src='/shard_0.png' />
+      </Box>
+    );
+  } else if (scepter == 1 && shard == 0) {
+    return (
+      <Box>
+        <Image src='/scepter_1.png' />
+        <Image src='/shard_0.png' />
+      </Box>
+    );
+  } else if (scepter == 0 && shard == 1) {
+    return (
+      <Box>
+        <Image src='/scepter_0.png' />
+        <Image src='/shard_1.png' />
+      </Box>
+    );
+  } else {
+    return (
+      <Box>
+        <Image src='/scepter_1.png' />
+        <Image src='/shard_1.png' />
+      </Box>
+    );
+  }
+}
+
+function makeNeutralBox(item) {
+  if (item.id == 0) {
+    return <Box bg='radial-gradient(#8D99AE, #212121);' w='2em' borderRadius={500} />;
+  } else {
+    return (
+      <Tooltip label={item.name}>
+        <Box>
+          <Image src={item.img} alt={item.name} objectFit='cover' overflow='hidden' w='2em' h='2em' borderRadius={500} />
         </Box>
       </Tooltip>
     );
