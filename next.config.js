ace8791def7c827a412fdd8c8199f048c53461b5
Connor Campbell <connorcam123@gmail.com>
2023-05-12 18:03:29 +0100

    Fixed next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/about",
-        destination: "/",
-        permanent: true,
-      },
-    ];
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
   },
-};
+}
 
-
-module.exports = nextConfig;
+module.exports = nextConfig
