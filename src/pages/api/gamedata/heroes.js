51da7e92f854266a4565acc11c2dad78a6fa2c65
connorcam302 <connorcam123@gmail.com>
2023-05-31 14:04:49 +0100

    Reverted change

--- src/pages/api/gamedata/heroes.js
+++ src/pages/api/gamedata/heroes.js
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 
 export const runtime = 'experimental-edge';
 
-export default function MyEdgeFunction(request, context) {
+export default async function handler(req, res) {
   let time = new Date();
 
   var heroes = [];
@@ -13,8 +13,23 @@ export default function MyEdgeFunction(request, context) {
   }
 
   if (heroes.length > 0) {
-    return NextResponse.json(heroes)
+    res.status(200).json(heroes);
   } else {
-    return NextResponse.json({status: 404, message: `Heroes file not found`});
+    res.status(404).json({ message: `Heroes file not found` });
   }
 }
+
+// export default function MyEdgeFunction(request, context) {
+//   let time = new Date();
+
+//   var heroes = [];
+//   for(let i = 0; i < heroesJson.length; i++) {
+//     heroes.push(heroesJson[i]);
+//   }
+
+//   if (heroes.length > 0) {
+//     return NextResponse.json(heroes)
+//   } else {
+//     return NextResponse.json({status: 404, message: `Heroes file not found`});
+//   }
+// }
