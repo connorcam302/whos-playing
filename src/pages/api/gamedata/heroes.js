9a815ae474361cd0271c525677a928b5962894a5
connorcam302 <connorcam123@gmail.com>
2023-05-31 23:19:33 +0100

    Testing edge

--- src/pages/api/gamedata/heroes.js
+++ src/pages/api/gamedata/heroes.js
@@ -1,10 +1,10 @@
 import heroesJson from "../../../data/heroes.json";
 
-// import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-// export const runtime = 'experimental-edge';
+export const runtime = 'experimental-edge';
 
-export default async function handler(req, res) {
+export default function MyEdgeFunction(request, context) {
   let time = new Date();
 
   var heroes = [];
@@ -13,23 +13,8 @@ export default async function handler(req, res) {
   }
 
   if (heroes.length > 0) {
-    res.status(200).json(heroes);
+    return NextResponse.json(heroes)
   } else {
-    res.status(404).json({ message: `Heroes file not found` });
+    return NextResponse.json({status: 404, message: `Heroes file not found`});
   }
 }
-
-// export default function MyEdgeFunction(request, context) {
-//   let time = new Date();
-
-//   var heroes = [];
-//   for(let i = 0; i < heroesJson.length; i++) {
-//     heroes.push(heroesJson[i]);
-//   }
-
-//   if (heroes.length > 0) {
-//     return NextResponse.json(heroes)
-//   } else {
-//     return NextResponse.json({status: 404, message: `Heroes file not found`});
-//   }
-// }
