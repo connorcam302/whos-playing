97ca4969a51e0ff4a6f99d99e5e5245e6f1db93f
Connor Campbell <connorcam123@gmail.com>
2023-10-11 16:24:13 +0100

    Optimised descending and bug fixes

--- src/pages/api/player/all.js
+++ src/pages/api/player/all.js
@@ -1,15 +1,15 @@
-const { createClient } = require("@supabase/supabase-js") 
+const { createClient } = require("@supabase/supabase-js")
 const supabase = createClient(process.env.SUPABASEURL, process.env.SUPABASEKEY);
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 export default async function handler({ query: { id } }, res) {
-  var player = await supabase.from("players").select("*")
+    var player = await supabase.from("players").select("*").order('username')
 
-  if(player.error !== null){
-    res.status(404).send({ message: `Player with id ${id} not found.`, status: 404 });
-  } else {
-    res.status(200).send(player.data)
-  }
+    if (player.error !== null) {
+        res.status(404).send({ message: `Player with id ${id} not found.`, status: 404 });
+    } else {
+        res.status(200).send(player.data)
+    }
 }
