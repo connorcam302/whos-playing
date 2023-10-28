94ea179c48f7e35f58dad6273362c72fbc1d2afe
connorcam302 <connorcam123@gmail.com>
2023-10-28 12:18:38 +0100

    Navbar rework

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -10,59 +10,59 @@ import LowestImpact from "./featureBoxes/LowestImpact";
 import Assists from "./featureBoxes/Assists";
 
 const Feature = (props) => {
-  const [descending, setDescending] = useState([]);
-  const fetchDescending = () => {
-    fetch(`/api/feature/descending`)
-      .then((res) => res.json())
-      .then((data) => {
-        setDescending(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  useEffect(() => {
-    fetchDescending();
-  }, []);
+    const [descending, setDescending] = useState([]);
+    const fetchDescending = () => {
+        fetch(`/api/feature/descending`)
+            .then((res) => res.json())
+            .then((data) => {
+                setDescending(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    useEffect(() => {
+        fetchDescending();
+    }, []);
 
-  const [image, setImage] = useState([]);
-  const fetchImage = () => {
-    fetch(`/api/splash-art`)
-      .then((res) => res.json())
-      .then((data) => {
-        setImage(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  useEffect(() => {
-    fetchImage();
-  }, []);
+    const [image, setImage] = useState([]);
+    const fetchImage = () => {
+        fetch(`/api/splash-art`)
+            .then((res) => res.json())
+            .then((data) => {
+                setImage(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    useEffect(() => {
+        fetchImage();
+    }, []);
 
-  return (
-    <Center boxShadow="0 5px 15px -2px rgba(0,0,0,.8)">
-      <Box
-        backgroundImage={`/api/splash-art`}
-        backgroundPosition={"center"}
-        w={"100%"}
-        h="35em"
-      >
-        <Center h={"100%"} backgroundColor={"rgba(0,0,0,0.75)"}>
-          <Wrap w={"80em"} spacing="50px" justify="center" padding="1em">
-            <Kills />
-            <Deaths />
-            <Impact />
-            <Climbing />
-            <GPM />
-            <Assists/>
-            <LowestImpact/>
-            <Descending />
-          </Wrap>
+    return (
+        <Center>
+            <Box
+                backgroundImage={`/api/splash-art`}
+                backgroundPosition={"center"}
+                w={"100%"}
+                h="35em"
+            >
+                <Center h={"100%"} backgroundColor={"rgba(0,0,0,0.75)"}>
+                    <Wrap w={"80em"} spacing="50px" justify="center" padding="1em">
+                        <Kills />
+                        <Deaths />
+                        <Impact />
+                        <Climbing />
+                        <GPM />
+                        <Assists />
+                        <LowestImpact />
+                        <Descending />
+                    </Wrap>
+                </Center>
+            </Box>
         </Center>
-      </Box>
-    </Center>
-  );
+    );
 };
 
 export default Feature;
